e72695c8e2ad5cb36d2ec603d6fe625f24ec1b1c
Ambyr-Shae Jarrell <arjarrel@uno.edu>
2018-05-01 15:09:40 -1000

    First commit
    Adding all files.
    Created greeting for home page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as anime from 'animejs';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app2.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
+
+  ngAfterViewInit() {
+    anime({
+      targets: '#name .lines path',
+      strokeDashoffset: [anime.setDashoffset, 0],
+      easing: 'linear',
+      duration: 2000,
+      delay: 1500
+    });
+  }
 }
