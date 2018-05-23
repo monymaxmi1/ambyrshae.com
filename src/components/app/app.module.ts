86ec709b9c220908af385ee77a94c8d858a55ed3
Ambyr-Shae Jarrell <arjarrel@uno.edu>
2018-05-22 16:00:23 -1000

    Added Angular Material

--- src/components/app/app.module.ts
+++ src/components/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 // Components
 import { AppComponent } from './app.component';
 import { GreetingComponent } from './../greeting/greeting.component';
+import { HomeComponent } from './../home/home.component';
+
+// Material
+import { MatButtonModule } from '@angular/material';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    GreetingComponent
+    GreetingComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
